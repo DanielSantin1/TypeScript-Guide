11d057974911912d169a856e24cdab4195fea08c
Daniel <danielsantin126264@gmail.com>
2025-12-27 11:01:56 -0300

    Aula 01 - Fundamentos do TypeScript

---
+++ src/aula01-fundamentos.ts
@@ -0,0 +1,19 @@
+let nome: string = "Daniel";
+let idade: number = 30;
+let ativo: boolean = true;
+
+console.log(nome, idade, ativo);
+
+
+let pontos = 10;
+pontos = 20;
+
+// pontos = "vinte"; 
+// se descomentar, dá erro
+
+
+let cidade = "Passo Fundo";
+let temperatura = 25;
+let chovendo = false;
+
+console.log('Cidade:', cidade, 'Temperatura:', temperatura+'º', 'Está Chovendo: ', chovendo);
